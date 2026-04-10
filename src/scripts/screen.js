9c2ff622f07211db54542c20a94fc21d275625af
ilonpilaajax <okisiitonen@gmail.com>
2026-04-11 00:38:56 +0300

    pohja tehty

---
+++ src/scripts/screen.js
@@ -0,0 +1,39 @@
+
+function devLog(logi){
+    console.log(logi)
+
+}
+
+
+function change_menu_state(menu_name, state){
+    
+    switch (menu_name) {
+        case "setting-menu":
+            const page_settings = document.getElementById("settings-menu");
+            page_settings.style.display = state
+        break;
+        case "user-menu":
+            const page_user = document.getElementById("user-menu");
+            page_user.style.display = state
+        break;
+        case "about-menu":
+            const page_about = document.getElementById("about-menu");
+            page_about.style.display = state
+        break;
+        case "power-menu":
+            const page_power = document.getElementById("power-menu");
+            page_power.style.display = state
+        break;
+    }
+}
+
+function set_power_state(power_type, state){
+    switch (power_type){
+
+
+
+
+    }
+
+
+}
